6af998f6a7209ad134b74e1733d59b5fb3a9ae6d
Umair Zafar <umairzafar164@gmail.com>
2021-07-02 01:14:31 +0500

    Cards added

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -17,7 +17,26 @@ const Photos = (props) => {
 
       <div>
         {photoList.map((m) => (
-         <div><img src={m.url}></img></div>
+          <div className="ui card">
+            <div className="image">
+              <img src={m.url}></img>
+            </div>
+            <div class="content">
+              <a class="header">Kristy</a>
+              <div class="meta">
+                <span class="date">Joined in 2013</span>
+              </div>
+              <div class="description">
+                Kristy is an art director living in New York.
+              </div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i class="user icon"></i>
+                22 Friends
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </div>
